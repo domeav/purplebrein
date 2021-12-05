bee57514a007bd494a399d1c5830eea44a3db755
Dominique Eav <dominique.eav@blue-mind.net>
2021-12-05 14:24:15 +0100

    Smarter collect

--- books/collect.sh
+++ books/collect.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-pdfunite ../*/C/*.pdf PurpleBrein_C.pdf
-pdfunite ../*/Bb/*.pdf PurpleBrein_Bb.pdf
-pdfunite ../*/Eb/*.pdf PurpleBrein_Eb.pdf
+
+pdfunite ../*/C/conductor.pdf PurpleBrein_conductor_C.pdf
+
+for key in C Bb Eb; do
+    pdfunite $(ls ../*/$key/{Bass_$key,*bassClef}.pdf) PurpleBrein_${key}_bassClef.pdf
+    pdfunite $(ls ../*/$key/{*[^Bass]_$key,*_${key}_trebleClef}.pdf) PurpleBrein_${key}_trebleClef.pdf
+done
