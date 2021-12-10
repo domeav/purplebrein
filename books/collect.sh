42755f931a36782bddfd408ba96a05729d2d20dd
Dominique Eav <dominique.eav@blue-mind.net>
2021-12-10 18:26:18 +0100

    Missing files in books

--- books/collect.sh
+++ books/collect.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+shopt -s extglob
 
 pdfunite ../*/C/conductor.pdf PurpleBrein_conductor_C.pdf
 
 for key in C Bb Eb; do
     pdfunite $(ls ../*/$key/{Bass_$key,*bassClef}.pdf) PurpleBrein_${key}_bassClef.pdf
-    pdfunite $(ls ../*/$key/{*[^Bass]_$key,*_${key}_trebleClef}.pdf) PurpleBrein_${key}_trebleClef.pdf
+    pdfunite $(ls ../*/$key/{!(Bass)_$key,*_${key}_trebleClef}.pdf) PurpleBrein_${key}_trebleClef.pdf
 done
