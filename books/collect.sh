cb294a48c910f7ce7ada188116939a3a537e3542
Dominique Eav <dominique.eav@blue-mind.net>
2022-10-05 16:13:27 +0200

    All pdfs generated

--- books/collect.sh
+++ books/collect.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 shopt -s extglob
 
+
+
+pdfunite ../*/C/conductor.pdf PurpleBrein_conductor_C.pdf
 pdfunite ../*/C/conductor.pdf PurpleBrein_conductor_C.pdf
 
 for key in C Bb Eb; do
-    pdfunite $(ls ../*/$key/{Bass_$key,*bassClef}.pdf) PurpleBrein_${key}_bassClef.pdf
-    pdfunite $(ls ../*/$key/{!(Bass)_$key,*_${key}_trebleClef}.pdf) PurpleBrein_${key}_trebleClef.pdf
+    pdfunite ../*/${key}/conductor.pdf PurpleBrein_conductor_${key}.pdf
+    pdfunite $(ls ../*/$key/*_F.pdf) PurpleBrein_${key}_F.pdf
+    pdfunite $(ls ../*/$key/*_F_pocket.pdf) PurpleBrein_${key}_F_pocket.pdf
+    pdfunite $(ls ../*/$key/*_G.pdf) PurpleBrein_${key}_G.pdf
+    pdfunite $(ls ../*/$key/*_G_pocket.pdf) PurpleBrein_${key}_G_pocket.pdf
 done
