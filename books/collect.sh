e00c52cdeb87d16a536fe3585ec4b10ada214865
Dominique Eav <dominique.eav@blue-mind.net>
2022-10-30 12:39:43 +0100

    Feat: pdf bookmarks

--- books/collect.sh
+++ books/collect.sh
@@ -3,13 +3,10 @@ shopt -s extglob
 
 
 
-pdfunite ../*/C/conductor.pdf PurpleBrein_conductor_C.pdf
-pdfunite ../*/C/conductor.pdf PurpleBrein_conductor_C.pdf
-
 for key in C Bb Eb; do
-    pdfunite ../*/${key}/conductor.pdf PurpleBrein_conductor_${key}.pdf
-    pdfunite $(ls ../*/$key/*_F.pdf) PurpleBrein_${key}_F.pdf
-    pdfunite $(ls ../*/$key/*_F_pocket.pdf) PurpleBrein_${key}_F_pocket.pdf
-    pdfunite $(ls ../*/$key/*_G.pdf) PurpleBrein_${key}_G.pdf
-    pdfunite $(ls ../*/$key/*_G_pocket.pdf) PurpleBrein_${key}_G_pocket.pdf
+    ./pdfmerger.py -o PurpleBrein_conductor_${key} ../*/${key}/conductor.pdf
+    ./pdfmerger.py -o PurpleBrein_${key}_F $(ls ../*/$key/*_F.pdf)
+    ./pdfmerger.py -o PurpleBrein_${key}_F_pocket $(ls ../*/$key/*_F_pocket.pdf)
+    ./pdfmerger.py -o PurpleBrein_${key}_G $(ls ../*/$key/*_G.pdf)
+    ./pdfmerger.py -o PurpleBrein_${key}_G_pocket $(ls ../*/$key/*_G_pocket.pdf)
 done
