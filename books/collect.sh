f017e59825e151ca328c62ee9426e3ffb40d62af
Dominique Eav <dominique.eav@blue-mind.net>
2023-09-12 16:06:39 +0200

    Add Bass Bb/F book

--- books/collect.sh
+++ books/collect.sh
@@ -9,4 +9,5 @@ for key in C Bb Eb; do
     ./pdfmerger.py -o PurpleBrein_${key}_F_pocket $(ls ../*/$key/*_F_pocket.pdf)
     ./pdfmerger.py -o PurpleBrein_${key}_G $(ls ../*/$key/*_G.pdf)
     ./pdfmerger.py -o PurpleBrein_${key}_G_pocket $(ls ../*/$key/*_G_pocket.pdf)
+    ./pdfmerger.py -o PurpleBrein_Bass_Bb_F_pocket $(ls ../*/Bb/Bass_F_pocket.pdf)
 done
